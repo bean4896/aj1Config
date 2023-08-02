6587964b3edc5dcd35966f423b191c34fdc1c296
KeYi <46948667+bean4896@users.noreply.github.com>
2023-08-02 15:54:32 +0800

    shadow inside main

--- root/Air.jsx
+++ root/Air.jsx
@@ -11,19 +11,19 @@ const Air = (props) => {
   return (
     <group {...props} dispose={null}>
       <group position={[-0.008, 0.769, 0.665]} rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
-        <mesh geometry={nodes.nikelogo.geometry} material={materials['mat_nikelogo.001']} position={[67.236, -66.482, 76.905]} />
-        <mesh geometry={nodes.top.geometry} material={materials['mat_top.001']} position={[-1.935, 10.163, -138.758]} />
-        <mesh geometry={nodes.xieBang.geometry} material={materials['mat_xieBang.001']} position={[0.78, 45.375, -26.845]} />
-        <mesh geometry={nodes.xiedai.geometry} material={materials['mat_xiedai.001']} position={[-5.082, 88.095, -78.736]} />
+        <mesh castShadow receiveShadow geometry={nodes.nikelogo.geometry} material={materials['mat_nikelogo.001']} position={[67.236, -66.482, 76.905]} />
+        <mesh castShadow receiveShadow  geometry={nodes.top.geometry} material={materials['mat_top.001']} position={[-1.935, 10.163, -138.758]} />
+        <mesh castShadow receiveShadow geometry={nodes.xieBang.geometry} material={materials['mat_xieBang.001']} position={[0.78, 45.375, -26.845]} />
+        <mesh castShadow receiveShadow geometry={nodes.xiedai.geometry} material={materials['mat_xiedai.001']} position={[-5.082, 88.095, -78.736]} />
         <group position={[0.78, 45.375, -26.842]}>
-          <mesh geometry={nodes.Mesh045.geometry} material={materials['mat_xiedi1.001']} />
-          <mesh geometry={nodes.Mesh045_1.geometry} material={materials['mat_xiedi2.001']} />
+          <mesh castShadow receiveShadow geometry={nodes.Mesh045.geometry} material={materials['mat_xiedi1.001']} />
+          <mesh castShadow receiveShadow geometry={nodes.Mesh045_1.geometry} material={materials['mat_xiedi2.001']} />
         </group>
-        <mesh geometry={nodes.xiemian.geometry} material={materials['mat_xiemian.001']} position={[48.369, 30.527, -50.576]} />
-        <mesh geometry={nodes.xieShe.geometry} material={materials['mat_xieshe.001']} position={[0.78, 45.375, -40.846]} />
-        <mesh geometry={nodes.xieTi.geometry} material={materials['mat_xieTi.001']} position={[-0.569, -66.464, 76.905]} />
+        <mesh castShadow receiveShadow geometry={nodes.xiemian.geometry} material={materials['mat_xiemian.001']} position={[48.369, 30.527, -50.576]} />
+        <mesh castShadow receiveShadow geometry={nodes.xieShe.geometry} material={materials['mat_xieshe.001']} position={[0.78, 45.375, -40.846]} />
+        <mesh castShadow receiveShadow geometry={nodes.xieTi.geometry} material={materials['mat_xieTi.001']} position={[-0.569, -66.464, 76.905]} />
       </group>
-      <mesh geometry={nodes.line.geometry} material={materials.Lines} position={[-0.002, 0.731, 0.771]} rotation={[Math.PI / 2, 0, 0]} scale={0.01} />
+      <mesh castShadow receiveShadow geometry={nodes.line.geometry} material={materials.Lines} position={[-0.002, 0.731, 0.771]} rotation={[Math.PI / 2, 0, 0]} scale={0.01} />
     </group>
   )
 }
--- root/src/components/Board.jsx
+++ root/src/components/Board.jsx
@@ -1,53 +1,53 @@
 // react three fiber documentation: https://github.com/pmndrs/drei#accumulativeshadows
-// https://codesandbox.io/s/environment-blur-and-transitions-forked-53lp2d?file=/src/App.js
-// https://codesandbox.io/s/stage-presets-forked-cg9vqm?file=/src/App.js
-// https://codesandbox.io/s/qxjoj?file=/src/App.js
+// https://codesandbox.io/s/environment-blur-and-transitions-forked-53lp2d?file=/src/App.js BasicSephere
+// https://codesandbox.io/s/stage-presets-forked-cg9vqm?file=/src/App.js Robot
+// https://codesandbox.io/s/qxjoj?file=/src/App.js  Shoe
 
-import { useState, useTransition } from 'react'
+import { useState } from 'react'
 import { Canvas } from "@react-three/fiber";
 import {
   Stage,
   PresentationControls,
-  MeshReflectorMaterial,
-  AccumulativeShadows,
-  RandomizedLight,
   Environment,
+  OrbitControls,
+  RandomizedLight,
   ContactShadows,
-  Backdrop
 } from "@react-three/drei";
 import Air from "../../Air";
 
+
 const Env = () => {
-  const [preset, setPreset] = useState('sunset');
-  return <Environment preset={preset} background={false} />
+  return <Environment preset='city' />
 };
 
 const Board = () => {
 
   return (
-    <Canvas shadows dpr={[1, 1.5]} camera={{ position: [4, -1, 8], fov: 45 }}>
-      <PresentationControls
+    <Canvas shadows dpr={[1, 1.5]} camera={{ position: [4, -1, 8], fov: 35 }}>
+      {/* <PresentationControls
         speed={1.5}
         global
         zoom={0.7}
         polar={[-0.1, Math.PI / 4]}
+      > */}
+
+      <Stage
+        intensity={0.5}
+        preset="rembrandt"
+        shadows={{ type: 'accumulative', color: '#f0f0f1', colorBlend: 2, opacity: 1 }}
+        adjustCamera={1}
+        environment="city"
       >
 
-        <Stage
-          intensity={0.5}
-          preset="rembrandt"
-          shadows={{ type: 'accumulative', color: 'skyblue', colorBlend: 2, opacity: 1 }}
-          adjustCamera={1}
-          environment="city">
+        {/* mesh */}
+        <mesh castShadow receiveShadow>
+          <Air />
+        </mesh>
 
-          <Env />
-
-          <mesh castShadow receiveShadow>
-            <Air />
-          </mesh>
-          <ContactShadows opacity={0.5} scale={1} blur={500} far={1} color="#000000" />
-        </Stage>
-      </PresentationControls>
+        {/* lighting */}
+      </Stage>
+      <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.9} makeDefault />
+      {/* </PresentationControls> */}
     </Canvas>
   );
 };
